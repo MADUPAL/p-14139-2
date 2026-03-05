c8b7840543ed6d3e3a851ee4ae7168a65adeb04b
MADUPAL <kevin488@naver.com>
2026-03-05 11:34:38 +0900

    test2

--- src/main/kotlin/com/back/BackApplication.kt
+++ src/main/kotlin/com/back/BackApplication.kt
@@ -2,7 +2,7 @@ package com.back
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-//
+
 @SpringBootApplication
 class BackApplication
 
